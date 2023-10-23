685eedd83f14b7e7c984bfe01a54486cd78f78a5
annagoetz1 <annalgoetz1@gmail.com>
2023-10-23 06:41:26 -0400

    Create rows for columns with another nested loop

--- etch-and-sketch.js
+++ etch-and-sketch.js
@@ -1,7 +1,15 @@
 const gridContainer = document.querySelector(".grid-container");
 
+for (let j = 0; j <16; j++) {
+    const row = document.createElement("div");
+    row.className = "grid-row";
+    
+
+
 for (let i = 0; i < 16; i++) {
     const gridSquare = document.createElement("div");
     gridSquare.className = "grid-square";
-   
-    gridContainer.appendChild(gridSquare); }
+   row.appendChild (gridSquare); }
+
+    gridContainer.appendChild(row);
+}
