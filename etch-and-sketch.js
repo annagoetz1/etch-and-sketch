3a55c5659c1800126d44c46b2ab7727b6c599d0c
annagoetz1 <annalgoetz1@gmail.com>
2023-10-23 06:20:33 -0400

    Remove textContent from function

--- etch-and-sketch.js
+++ etch-and-sketch.js
@@ -3,5 +3,5 @@ const gridContainer = document.querySelector(".grid-container");
 for (let i = 0; i < 16; i++) {
     const gridSquare = document.createElement("div");
     gridSquare.className = "grid-square";
-    gridSquare.textContent = i + 1;
+   
     gridContainer.appendChild(gridSquare); }
