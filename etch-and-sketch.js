a66f40efd23a4c9d0ddc6881b5e44e0527f75340
annagoetz1 <annalgoetz1@gmail.com>
2023-10-30 06:46:10 -0400

    Add mousein and mouseout event for hover on divs

--- etch-and-sketch.js
+++ etch-and-sketch.js
@@ -37,3 +37,10 @@ for (let i = 0; i < numberOfSquares; i++) {
     gridContainer.appendChild(row);
 }
 }
+const gridHover = document.querySelectorAll ('div');
+div.addEventListener ('mouseover', () => {
+    div.style.backgroundColor = 'blue';
+});
+div.addEventListener ('mouseout', () => {
+div.style.backgroundColor = '';
+});
